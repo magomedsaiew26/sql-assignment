44fe13706116c8c2e69cba1ef0712d55421f694f
Bror Nydal <47297059+BrorNydal@users.noreply.github.com>
2023-02-20 15:54:05 +0100

    feat: customer repository interface

--- DataAccess/Repositories/ICustomerRepository.cs
+++ DataAccess/Repositories/ICustomerRepository.cs
@@ -1,2 +1,13 @@
 ﻿
-//TODO: Create interface
+using DataAccess.Models;
+
+public interface ICustomerRepository
+{
+    public IList<Customer> GetAll();
+    public IList<Customer> GetRange(int offset, int limit);
+    public Customer Get(int id);
+    public Customer Get(string name);    
+    public bool Add(Customer customer);
+    public bool Update(int id, Customer updatedCustomer);
+
+}
