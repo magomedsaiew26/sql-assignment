f03a4088ebd0a6feec544ca0520391215427a5a3
Bror Nydal <47297059+BrorNydal@users.noreply.github.com>
2023-02-20 14:07:30 +0100

    feat: assistant value insertion script

---
+++ SqlScripts/06_insertAssistants.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO Assistant ([Name], SuperheroId)
+VALUES ('Tuxedo Mask', 1);
+
+INSERT INTO Assistant ([Name], SuperheroId)
+VALUES ('Taric', 2);
+
+INSERT INTO Assistant ([Name], SuperheroId)
+VALUES ('Diddy Kong', 3);
