a0b0546d47a34a8d3876631f91ea205ddbec2dcb
Bror Nydal <47297059+BrorNydal@users.noreply.github.com>
2023-02-20 14:07:43 +0100

    feat: power value insertion script

---
+++ SqlScripts/07_powers.sql
@@ -0,0 +1,27 @@
+
+INSERT INTO [Power] ([Name], [Description])
+VALUES ('Friendship', 'Trumphs evil');
+
+INSERT INTO [Power] ([Name], [Description])
+VALUES ('Curtains call', 'Calls the curtain');
+
+INSERT INTO [Power] ([Name], [Description])
+VALUES ('Throw banana', 'Throws banana');
+
+INSERT INTO [Power] ([Name], [Description])
+VALUES ('Groundstomp', 'Stomps the ground');
+
+INSERT INTO SuperHeroPower (SuperheroId, PowerId)
+VALUES (1, 1);
+
+INSERT INTO SuperHeroPower (SuperheroId, PowerId)
+VALUES (2, 2);
+
+INSERT INTO SuperHeroPower (SuperheroId, PowerId)
+VALUES (3, 3);
+
+INSERT INTO SuperHeroPower (SuperheroId, PowerId)
+VALUES (3, 2);
+
+INSERT INTO SuperHeroPower (SuperheroId, PowerId)
+VALUES (3, 1);
