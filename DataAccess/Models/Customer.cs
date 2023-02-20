8f3c5d45de15f9627a9d1e0317743f848086757a
Bror Nydal <47297059+BrorNydal@users.noreply.github.com>
2023-02-20 15:34:41 +0100

    feat: customer model

--- DataAccess/Models/Customer.cs
+++ DataAccess/Models/Customer.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace DataAccess.Models
 {
-    public record Customer(int Id, string FirstName, string LastName, 
+    public record struct Customer(int Id, string FirstName, string LastName, 
         string Country, string PostalCode, 
         string Phonenumber, string Email);
 }
--- DataAccess/Repositories/CustomerRepository.cs
+++ DataAccess/Repositories/CustomerRepository.cs
@@ -1,9 +1,17 @@
 ﻿
+using DataAccess.Models;
 
 namespace DataAccess.Repositories
 {
     public class CustomerRepository
     {
         //TODO: GetAll, Get, GetLimited, Delete, Update...
+
+        public static List<Customer> GetAll()
+        {
+            List<Customer> result = new List<Customer>();
+
+
+        }
     }
 }
