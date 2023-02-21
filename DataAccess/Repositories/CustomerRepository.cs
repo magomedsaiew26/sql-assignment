d2e5c04628c9a629ae4b8653b8c56be3fea598d2
magomedsaiew26 <122887078+magomedsaiew26@users.noreply.github.com>
2023-02-21 14:20:17 +0100

    Add files via upload

--- DataAccess/Repositories/CustomerRepository.cs
+++ DataAccess/Repositories/CustomerRepository.cs
@@ -1,8 +1,6 @@
-﻿
-using DataAccess.Models;
+﻿using DataAccess.Models;
 using Microsoft.Data.SqlClient;
-using System.Numerics;
-using System.Text;
+using System.Diagnostics.Metrics;
 
 namespace DataAccess.Repositories
 {
@@ -10,81 +8,156 @@ namespace DataAccess.Repositories
     {
         //TODO: GetAll, Get, GetLimited, Delete, Update...
 
+        private static string ServerName => "N-NO-01-01-1467\\SQLEXPRESS02";
+        private static string DatabaseName = "Chinook";
         private static string ConncectionString
         {
             get
             {
                 SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-                builder.DataSource = "";
+                builder.DataSource = ServerName;
+                builder.InitialCatalog = DatabaseName;
                 builder.IntegratedSecurity = true;
                 builder.TrustServerCertificate = true;
                 return builder.ConnectionString;
             }
         }
 
-        public IList<Customer> GetAll()
+        private void SelectQuery(string query, System.Predicate<SqlDataReader> predicate)
         {
-            IList<Customer> result = new List<Customer>();
-
             //setup connection
             using (SqlConnection connection = new SqlConnection(ConncectionString))
             {
                 connection.Open();
-
-                string sqlString = "SELECT * FROM Customer";
 
                 //create command
                 using (SqlCommand command = connection.CreateCommand())
                 {
-                    command.CommandText = sqlString; //set command
+                    command.CommandText = query; //set command
 
                     //reads the result of the command
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
-                        while (reader.Read())
-                        {
-                            result.Add(new Customer
-                            {
-                                Id = reader.GetInt32(0),
-                                FirstName = reader.GetString(1),
-                                LastName = reader.GetString(2),
-                                Country = reader.GetString(3),
-                                PostalCode = reader.GetString(4),
-                                Phonenumber = reader.GetString(5),
-                                Email = reader.GetString(6)
-                            });
-                        }
+                        predicate?.Invoke(reader);
                     }
                 }
             }
-
-            return result;
         }
 
-        public IList<Customer> GetRange(int offset, int limit)
+        public Task<IList<Customer>> GetAll()
         {
-            throw new NotImplementedException();
+            IList<Customer> result = new List<Customer>();
+
+            SelectQuery("SELECT * FROM Chinook.dbo.Customer;", (reader) =>
+            {
+                while (reader.Read())
+                {
+                    var customer = new Customer
+                    {
+                        Id = reader.GetInt32(0),
+                        FirstName = reader.GetString(1),
+                        LastName = reader.GetString(2)
+                    };
+
+                    if (!reader.IsDBNull(7))
+                        customer.Country = reader.GetString(7);
+                    if (!reader.IsDBNull(8))
+                        customer.PostalCode = reader.GetString(8);
+                    if (!reader.IsDBNull(9))
+                        customer.Phonenumber = reader.GetString(9);
+                    if (!reader.IsDBNull(11))
+                        customer.Email = reader.GetString(11);
+
+                    result.Add(customer);
+                }
+
+                return true;
+            });
+
+            return Task.FromResult(result);
         }
 
-        public Customer Get(int id)
+        public Task<IList<Customer>> GetRange(int offset, int limit)
         {
-            throw new NotImplementedException();
+            IList<Customer> result = new List<Customer>();
+
+            SelectQuery($"SELECT * FROM Chinook.dbo.Customer ORDER BY CustomerId OFFSET {offset} ROWS FETCH NEXT {limit} ROWS ONLY;", (reader) =>
+            {
+                while (reader.Read())
+                {
+                    var customer = new Customer
+                    {
+                        Id = reader.GetInt32(0),
+                        FirstName = reader.GetString(1),
+                        LastName = reader.GetString(2)
+                    };
+
+                    if (!reader.IsDBNull(7))
+                        customer.Country = reader.GetString(7);
+                    if (!reader.IsDBNull(8))
+                        customer.PostalCode = reader.GetString(8);
+                    if (!reader.IsDBNull(9))
+                        customer.Phonenumber = reader.GetString(9);
+                    if (!reader.IsDBNull(11))
+                        customer.Email = reader.GetString(11);
+
+                    result.Add(customer);
+                }
+
+                return true;
+            });
+
+            return Task.FromResult(result);
         }
 
-        public Customer Get(string name)
+        public Task<Customer> Get(int id)
         {
-            throw new NotImplementedException();
+            IList<Customer> result = new List<Customer>();
+
+            SelectQuery($"SELECT * FROM Chinook.dbo.Customer WHERE CustomerId = {id};", (reader) =>
+            {
+                while (reader.Read())
+                {
+                    var customer = new Customer
+                    {
+                        Id = reader.GetInt32(0),
+                        FirstName = reader.GetString(1),
+                        LastName = reader.GetString(2)
+                    };
+
+                    if (!reader.IsDBNull(7))
+                        customer.Country = reader.GetString(7);
+                    if (!reader.IsDBNull(8))
+                        customer.PostalCode = reader.GetString(8);
+                    if (!reader.IsDBNull(9))
+                        customer.Phonenumber = reader.GetString(9);
+                    if (!reader.IsDBNull(11))
+                        customer.Email = reader.GetString(11);
+
+                    result.Add(customer);
+                }
+
+                return true;
+            });
+
+            if (result.Count > 1)
+                Console.WriteLine("Recieved multiple results from a single id");
+            if (result.Count == 0)
+            {
+                Console.WriteLine("No results!");
+                return Task.FromResult(default(Customer));
+            }
+            return Task.FromResult(result[0]);
         }
 
-        public bool Add(Customer customer)
-        {
-            // 5. Add a new customer to the database
-            //setup connection
-            using (SqlConnection connection = new SqlConnection(ConncectionString))
+        // 5. Add a new customer to the database
+        //setup connection
+        using (SqlConnection connection = new SqlConnection(ConncectionString))
             {
                 connection.Open();
 
-                StringBuilder sqlStringql = new StringBuilder("INSERT INTO Customer(FirstName, LastName, Company, Address, City, State, Country, ");
+                //
+                StringBuilder sqlString = new StringBuilder("INSERT INTO Customer(FirstName, LastName, Company, Address, City, State, Country, ");
 
 
                 sqlString.Append("Postal Code, Phine, Fax, Email, SupportRepId) VALUES(";
@@ -94,7 +167,7 @@ namespace DataAccess.Repositories
                 sqlString.Append(customer.LastName);
                 customer.Append(", ");
                 sqlString.Append(customer.Country);
-                customer.Append(", "););
+                customer.Append(", ");
                 sqlString.Append(customer.PostalCode);
                 customer.Append(", ");
                 sqlString.Append(customer.Phone);
@@ -169,15 +242,73 @@ namespace DataAccess.Repositories
 
         public IList<String, int> CustomersByCountry()
         {
-            // 7. Return the number of customers in each country, ordered high to low
+                // 7. Return the number of customers in each country, ordered high to low
+                IList<CountryNumber> result = new List<CountryNumber>();
 
-            throw new NotImplementedException();
-        }
-        public IList<Customer> HighestSpenders(int n)
+                //setup connection
+                using (SqlConnection connection = new SqlConnection(ConncectionString))
+                {
+                    connection.Open();
+
+                    string sqlString = "SELECT Country, count(*) FROM Customer GROUP BY Country";
+
+                    //create command
+                    using (SqlCommand command = connection.CreateCommand())
+                    {
+                        command.CommandText = sqlString; //set command
+
+                        //reads the result of the command
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                result.Add(new CountryNumber
+                                {
+                                    country = reader.GetString(0),
+                                    count = Convert.ToInt64(reader.GetString(1))
+                                });
+                            }
+                        }
+                    }
+                }
+
+                return result;
+
+                // throw new NotImplementedException();
+            }
+            public IList<Customer> HighestSpenders(int n)
         {
-            // 8. Return a list of the n highest spenders 
+                // 8. Return a list of the n highest spenders 
+                    string sqlString = = "SELECT c.CustomerId, c.FirstName, c.LastName, SUM(i.Total) as TotalSpent " +
+                             "FROM Customer c " +
+                             "INNER JOIN Invoice i ON c.CustomerId = i.CustomerId " +
+                             "GROUP BY c.CustomerId, c.FirstName, c.LastName " +
+                             "ORDER BY TotalSpent DESC";
+                //Setup connection
 
-            throw new NotImplementedException();
+                using (SqlConnection connection = new SqlConnection(ConncectionString))
+                {
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        int customerId = reader.GetInt32(0);
+                        string firstName = reader.GetString(1);
+                        string lastName = reader.GetString(2);
+                        decimal totalSpent = reader.GetDecimal(3);
+
+                        Console.WriteLine("{0}: {1} {2} - {3}", customerId, firstName, lastName, totalSpent);
+                    }
+
+                    reader.Close();
+                }
+            }
+        }
+
+
+                    throw new NotImplementedException();
         }
         public String MostPopularGenre(int id)
         {
